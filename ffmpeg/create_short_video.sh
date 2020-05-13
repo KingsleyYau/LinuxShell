c29f1bd6ea20d9430af6daf8c8bcc8850d1976a0
Kingsley Yau <Kingsleyyau@gmail.com>
2020-05-13 11:40:28 +0800

    同步

--- ffmpeg/create_short_video.sh
+++ ffmpeg/create_short_video.sh
@@ -15,11 +15,18 @@ then
 fi
 
 FILE="$1"
-START=110
+START=0
 #$FFMPEG -i /Users/max/Documents/log/LS/video/ajypj.mp4 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//
-for((i=0;i<=50;i++));  
+for((i=0,start=0;i<=10;i++));  
 do  
 	echo $i; 
-	$FFMPEG -i $FILE -vcodec libx264 -preset superfast -profile:v baseline -level 3.0 -acodec copy -ss 00:00:$i -t 00:00:10 output_$(expr $START + $i).mp4
+	second=$(expr $start % 60);
+	min=$(expr $start / 60);
+	
+	start=$(expr $start + 30)
+	#second_new=$(expr $start % 60);
+	#min_new=$(expr $start / 60);
+	
+	$FFMPEG -i $FILE -vcodec libx264 -preset superfast -profile:v baseline -level 3.0 -acodec copy -ss 00:$min:$second -t 00:00:30 output_$(expr $START + $i).mp4
 done  
 #$FFMPEG -i $FILE -vcodec libx264 -preset superfast -profile:v baseline -level 3.0 -acodec copy -ss 00:00:10 -t 00:00:10 1.mp4#
