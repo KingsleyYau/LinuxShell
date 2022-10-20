c53d4682eb51f271c119680e04135cb0a2f96be9
Kingsley Yau <Kingsleyyau@gmail.com>
2022-10-20 16:29:57 +0800

    Update mv_video.sh

--- ffmpeg/mv_video.sh
+++ ffmpeg/mv_video.sh
@@ -19,9 +19,4 @@ if [ "$2" != "" ];then
 fi
 
 MV_PATH="input/${NAME}"
-find download -name "${NAME}*" -type d | xargs -I {} echo {} | awk '{cmd="echo "$0"|md5sum|cut -d \" \" -f1";cmd|getline ret;print ret" "$0}' | awk '{name="input/""'${NAME}_'"$1;cmd="mv "$2" "name;cmd|getline;print name;}' | xargs -I {} ./sort_mp4_jpg.sh {} "${OUTPUT}/${NAME}_S"
-
-#if [ "$MV_PATH" != "input" ] || [ "$MV_PATH" != "input/" ];then
-  #echo "$MV_PATH"
-  #rm "$MV_PATH" -rf
-#fi
+find download -name "${NAME}*" -type d | awk '{cmd="echo \""$0"\"|md5sum|cut -d \" \" -f1";cmd|getline ret;print ret","$0}' | awk -F ',' '{name="input/""'${NAME}_'"$1;cmd="mv \""$2"\" "name;cmd|getline ret;print name;}' | xargs -I {} ./sort_mp4_jpg.sh {} "${OUTPUT}/${NAME}_S"
